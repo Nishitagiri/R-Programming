d9957b2c9aa7b72a91aa9b35bc240bdd033ed000
Nishitagiri <girinishitacs242508@gmail.com>
2026-01-05 11:01:20 +0530

    Update S080_9_mod2.R

--- S080_9_mod2.R
+++ S080_9_mod2.R
@@ -1,22 +1,17 @@
-# ============================================
-# Chi-square Test using CSV file
-# ============================================
 
-# Load required library
 library(stats)
 
-# Read the CSV file
 df <- read.csv("~/Nishita/test_data.csv")
 
-# View first few rows
 head(df)
 
-# Create contingency table (Sex vs Survived)
+#create table (Sex vs Survived)
 chi_table <- table(df$Sex, df$Survived)
 
-# Display the table
+#display table
 chi_table
 
-# Perform Chi-square test
+#perform Chi-square test
 chisq.test(chi_table)
 print("Nishita Giri S080")
+
