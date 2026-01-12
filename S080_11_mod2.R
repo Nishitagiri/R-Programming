2b27d601ebc0c1fe0e6b73fd9c419087d6963f48
Nishitagiri <girinishitacs242508@gmail.com>
2026-01-12 12:15:19 +0530

    Add files via upload

--- S080_11_mod2.R
+++ S080_11_mod2.R
@@ -1,17 +1,24 @@
-
 library(ggplot2)
 
-# Read dataset (C:/Users/info/Downloads/heart_failure_clinical_records_dataset.csv)
+#for reading the dataset
 csv <- read.csv("C:/Users/info/Downloads/heart_failure_clinical_records_dataset.csv")
 
-ggplot(sv, aes(x = age)) +
-  geom_histogram(binwidth = 5, fill = "pink", color = "purple") +
-  labs(title = "Histogram of Age S080", x = "Age", y = "Frequency") +
+
+ggplot(csv, aes(x = age)) +
+  geom_histogram(binwidth = 5, fill = "lightpink", color = "mistyrose") +
+  labs(
+    title = "Histogram of Age S080",
+    x = "Age",
+    y = "Frequency"
+  ) +
   theme_minimal()
 
-ggplot(sv, aes(x = factor(DEATH_EVENT), y = serum_creatinine)) +
-  geom_boxplot(fill = "grey") +
-  labs(title = "Serum Creatinine by Death Event S080",
-       x = "Death Event",
-       y = "Serum Creatinine") +
+
+ggplot(csv, aes(x = factor(DEATH_EVENT), y = serum_creatinine)) +
+  geom_boxplot(fill = "lavender") +
+  labs(
+    title = "Serum Creatinine by Death Event S080",
+    x = "Death Event",
+    y = "Serum Creatinine"
+  ) +
   theme_minimal()
